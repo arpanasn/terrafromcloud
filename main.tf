a5502b22573c3d2744cf4438c0346c0c62ddd32e
arpanasn <81707949+arpanasn@users.noreply.github.com>
2023-09-11 14:23:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,4 +23,12 @@ resource "azurerm_subnet" "subnet" {
       azurerm_virtual_network.network
     ]
 }
-
+resource "azurerm_subnet" "subnet" {    
+    name                 = "SubnetB"
+    resource_group_name  = local.resource_group_name
+    virtual_network_name = local.virtual_network.name
+    address_prefixes     = [cidrsubnet(local.virtual_network.address_space,8,0)]
+    depends_on = [
+      azurerm_virtual_network.network
+    ]
+}
